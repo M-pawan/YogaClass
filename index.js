4a5f049fe8f9ca3243f5e4f96d72facadf24e87e
Pawan <pawan.kumar1733@gmail.com>
2022-12-12 23:50:14 +0530

    depolyment error solve

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", function (req, res) {
   res.sendFile(
-    "./client/build/index.html",
+    path.join(__dirname, "./client/build/index.html"),
     function (err) {
       res.status(500).send(err);
     }
