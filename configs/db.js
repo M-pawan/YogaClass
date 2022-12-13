c952334dd86b9e49b0d828f5b176bf4d05ef0dde
Pawan <pawan.kumar1733@gmail.com>
2022-12-13 17:43:53 +0530

    change in gitignore

--- configs/db.js
+++ configs/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 var pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASS,
-  database: process.env.MYSQL_DB,
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
   connectionLimit: 10,
   supportBigNumbers: true,
 });
